369889a560138d1ef6cdeb524ba415f849e1d3b3
prakashkuruba <prakashkuruba111@gmail.com>
2024-04-15 18:56:34 +0530

    sumofmath

---
+++ basic/Math.js
@@ -0,0 +1,11 @@
+
+
+const sum=(n)=>{
+    let s=0;
+    for(let i=1;i<=n;i++){
+        s+=i;
+    }    
+    return s;
+}
+
+module.exports = sum;
--- basic/index.js
+++ basic/index.js
@@ -3,6 +3,43 @@
 
 const express= require("express");
 const cors=require("cors");
+const dotenv = require("dotenv");
 
-const port = 
+dotenv.config()
+
+
+
+const port = process.env.PORT || 3000; 
 const app = express();
+const sum=require("./Math.js");
+
+app.use(cors())
+
+app.get("/",(req,res)=>res.json({Message : "server is started 💻🖥️🧑‍💻"}));
+
+app.get("/home",(req,res)=>{
+    res.send("home route 🏡🏠")
+})
+
+app.get("/fruits",(req,res)=>{
+    res.json({
+        fruits: "🥭🥭🍏🍎"
+    })
+})
+
+
+
+app.get("/s",(req,res)=>{
+    const v=req.query.value;
+    const result = sum(v);
+    res.json({
+        sum: ` sum of ${v} natural numbers : ${result} ` 
+    })
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`\n\t Server is runnig on the port : ${port }`)
+});
+
